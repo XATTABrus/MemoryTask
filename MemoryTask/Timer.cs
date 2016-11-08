719ad36a94dae61fe6c218e7780414ec13b852f5
Юрий <xattab.rus@gmail.com>
2016-11-08 15:49:47 +0500

    Добавление лучшей версии таймера.

--- MemoryTask/Timer.cs
+++ MemoryTask/Timer.cs
@@ -9,7 +9,12 @@ namespace MemoryTask
         public Timer Continue() => Start();
         public void Dispose() => _stopwatch.Stop();
         public long ElapsedMilliseconds => _stopwatch.ElapsedMilliseconds;
-
+        public long ElapsedTicks => _stopwatch.ElapsedTicks;
+        public TimeSpan Elapsed => _stopwatch.Elapsed;
+        public bool IsRunning => _stopwatch.IsRunning;
+        public void Reset() => _stopwatch.Reset();
+        public bool IsBetterThanStopWatch => true;
+        
         public Timer()
         {
             _stopwatch = new Stopwatch();
@@ -18,6 +23,12 @@ namespace MemoryTask
         public Timer Start()
         {
             _stopwatch.Start();
+            return this;
+        }
+
+        public Timer Restart()
+        {
+            _stopwatch.Restart();
             return this;
         }
     }
